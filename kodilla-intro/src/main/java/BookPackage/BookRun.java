c0251a9843054461af725cf7c510fb0f5c6bbdfc
alibersk <alibersk@gmail.com>
2024-09-22 00:07:38 +0200

    Book Exercise 2.7 update

--- kodilla-intro/src/main/java/BookPackage/BookRun.java
+++ kodilla-intro/src/main/java/BookPackage/BookRun.java
@@ -2,7 +2,7 @@ package BookPackage;
 
 public class BookRun {
     public static void main(String[] args) {
-        String book = Book.of("Stanislaw Lem", "Golem XIV");
+        Book.of("Stanislaw Lem", "Golem XIV");
     }
 
 }
